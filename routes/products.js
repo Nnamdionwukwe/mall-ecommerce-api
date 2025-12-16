87d8d4e54c0c301aea02a98bdb54f2a194dbe647
Onwukwennamdi <nnamdionwukwe@gmail.com>
2025-12-16 11:32:16 +0100

    efwefew

--- routes/products.js
+++ routes/products.js
@@ -201,7 +201,7 @@ router.get("/", async (req, res) => {
       search,
       isActive,
       page = 1,
-      limit = 1000,
+      limit = 10000,
       sortBy = "createdAt",
       order = "desc",
     } = req.query;
