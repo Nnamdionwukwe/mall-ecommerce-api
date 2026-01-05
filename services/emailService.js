717eade1f449fbc45687416700f42e6299208277
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-01-05 23:49:01 +0100

    dwfvdw

--- services/emailService.js
+++ services/emailService.js
@@ -11,10 +11,10 @@ const {
 
 // Initialize email transporter
 const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE || "gmail",
+  service: process.env.GMAIL_SERVICE || "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD,
   },
 });
 
